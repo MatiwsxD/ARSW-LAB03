67afacf5e2eccc7f82db06f1fc9464c9f57a9bd1
unknown <juan.mejia-z@labinfo.is.escuelaing.edu.co>
2022-02-02 09:44:56 -0500

    Primer punto lab

---
+++ src/main/java/edu/eci/arst/concprg/prodcons/Consumer.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.arst.concprg.prodcons;
+
+import java.util.Queue;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author hcadavid
+ */
+public class Consumer extends Thread{
+    
+    private Queue<Integer> queue;
+    
+    
+    public Consumer(Queue<Integer> queue){
+        this.queue=queue;        
+    }
+    
+    @Override
+    public void run() {
+        while (true) {
+            synchronized (queue) {
+                if (queue.size() > 0) {
+                    int elem = queue.poll();
+                    System.out.println("Consumer consumes " + elem);
+                    notifyAll();
+                } else {
+                    /**try {
+                        queue.wait();
+                    } catch (InterruptedException ex) {
+                        System.out.println("1");
+                        Logger.getLogger(Producer.class.getName()).log(Level.SEVERE, null, ex);
+                    */
+                }
+            }
+            
+        }
+    }
+}
